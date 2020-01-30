cf0c265e77ac071c2fe717aa7623b28a870203c7
Christine-N-Mwaura <njerimwaura25@gmail.com>
2020-01-30 12:18:58 +0300

    add logic for opening and closing sidebar.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
-function openSlideMenu(){
-    document.getElementById('menu').style.width='250px';
-    document.getElementById('content').style.marginLeft = '250px'; 
-} 
-function closeSideMenu(){
-    document.getElementById('menu').style.width='0px';
-    document.getElementById('content').style.marginLeft = '0px'; 
-} 
+ 
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
 
