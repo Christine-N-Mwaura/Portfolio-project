9be4eb6c516be8197206e69c307f4f7d70a1ca92
Christine-N-Mwaura <njerimwaura25@gmail.com>
2020-02-17 21:42:24 +0300

    add toggle logic for the education section

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,39 @@
  
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
+// function openNav() {
+//     document.getElementById("mySidebar").style.width = "250px";
+//     document.getElementById("main").style.marginLeft = "250px";
+//   }
   
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
+//   function closeNav() {
+//     document.getElementById("mySidebar").style.width = "0";
+//     document.getElementById("main").style.marginLeft= "0";
+//   }
+$(document).ready(function(){
+  $(".one").click(function(){
+    $(".panel-body.one").toggle('slow');
+    $(".panel-title.one a").toggleClass("collapsed");
+    
+  });
 
+  $(".two").click(function(){
+    $(".panel-body.two").toggle('slow');
+    $(".panel-title.two a").toggleClass("collapsed");
+    
+  });
+
+  $(".three").click(function(){
+    $(".panel-body.three").toggle('slow');
+    $(".panel-title.three a").toggleClass("collapsed");
+    
+  });
+  $(".four").click(function(){
+    $(".panel-body.four").toggle('slow');
+    $(".panel-title.four a").toggleClass("collapsed");
+    
+  });
+  $(".five").click(function(){
+    $(".panel-body.five").toggle('slow');
+    $(".panel-title.five a").toggleClass("collapsed");
+    
+  });
+});
